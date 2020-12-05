28aac082fab9eedbb0b61130140312a10d72ab32
Jorge Rosero <jroseros@ucenfotec.ac.cr>
2020-12-05 00:11:36 -0600

    second commit

---
+++ Dump20201204/sys_cancion.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sys
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cancion`
+--
+
+DROP TABLE IF EXISTS `cancion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cancion` (
+  `idcancion` int NOT NULL AUTO_INCREMENT,
+  `nombrecancion` varchar(45) NOT NULL,
+  `generos` int NOT NULL,
+  `compositor` int NOT NULL,
+  `fechalanzamiento` date NOT NULL,
+  PRIMARY KEY (`idcancion`),
+  KEY `idcanciongenero_idx` (`generos`),
+  KEY `idcancioncompositor_idx` (`compositor`),
+  CONSTRAINT `idcancioncompositor` FOREIGN KEY (`compositor`) REFERENCES `compositor` (`idcompositor`),
+  CONSTRAINT `idcanciongenero` FOREIGN KEY (`generos`) REFERENCES `genero` (`idgenero`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cancion`
+--
+
+LOCK TABLES `cancion` WRITE;
+/*!40000 ALTER TABLE `cancion` DISABLE KEYS */;
+INSERT INTO `cancion` VALUES (1,'pedo',1,1,'2000-03-20'),(2,'otra',1,1,'2000-03-20'),(3,'pfg',2,1,'2000-03-21'),(4,'I love tacos',2,1,'2020-02-21'),(5,'ss2',1,1,'2000-10-10'),(6,'ww',1,1,'2000-11-11'),(7,'22222s',1,1,'2000-11-11'),(8,'nuevacancion',2,1,'2001-11-11');
+/*!40000 ALTER TABLE `cancion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-04 23:17:28
